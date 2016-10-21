611a7648d81fd478027a39cfa1b28e0668f718c6
markspencertan <macbonkaynel@gmail.com>
2016-10-20 18:05:25 -0700

    updated js to update fields.

--- js/foodisco.js
+++ js/foodisco.js
@@ -52,9 +52,34 @@ function updateRestaurant(jsonobj){
     $RESTAURANT_OBJ = $restaurants[$pos];
     $image = $RESTAURANT_OBJ.image_url;
     $name = $RESTAURANT_OBJ.name;
+    $phone = $RESTAURANT_OBJ.phone;
+    $phone = "("+$phone.substr(0,3)+") "+$phone.substr(3,3) + "-"+ $phone.substr(6);
+    $addressObject = $RESTAURANT_OBJ.location.address;
+    $city = $RESTAURANT_OBJ.location.city;
+    $rating = $RESTAURANT_OBJ.rating_img_url_large;
+    $categories = $RESTAURANT_OBJ.categories;
+    $url = $RESTAURANT_OBJ.url;
+
+	$streetAddress = "";
+
+	for (var i = 0; i < $addressObject.length; i++) {
+   		$streetAddress += $addressObject[i] +"\n";
+	}
+    $streetAddress += $city;
+    $categories_content = $categories[0][0] ;
+    if($categories.length > 1){
+        $categories_content += ", "+$categories[1][0];
+    }
     
     $("#name").html($name);
     $("#image").attr('src',$image);
+    $("#rating").attr('src', $rating);
+    $("#img-link").attr('href', $url);
+    $("#see-more").attr('href', $url);
+    $("#address").text($streetAddress);
+    $("#phone").text($phone);
+    $("#category").text($categories_content);
+    
     
     //Collapses The Output Div
     $("#output").collapse("toggle");
@@ -62,4 +87,5 @@ function updateRestaurant(jsonobj){
     
     //replaces Search Button to Disco Again
     $("#search").text("Disco Again");
+    
 }
